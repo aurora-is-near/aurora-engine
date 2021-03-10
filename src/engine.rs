33a817368847e6a9fe618358a03efb45a0b6108b
Arto Bendiken <arto@near.org>
2021-03-10 01:46:05 +0000

    Add engine state stubs.
    Co-authored-by: Illia Polosukhin <ilblackdragon@gmail.com>

--- src/engine.rs
+++ src/engine.rs
@@ -3,6 +3,8 @@ use alloc::vec::Vec;
 #[cfg(feature = "std")]
 use std::vec::Vec;
 
+use crate::types::{FunctionCallArgs, ViewCallArgs};
+use evm::{ExitReason, ExitSucceed};
 use primitive_types::{H160, H256, U256};
 
 pub struct Engine {
@@ -16,5 +18,64 @@ impl Engine {
             chain_id: U256::from(chain_id),
             origin,
         }
+    }
+
+    pub fn set_code(address: &H160, code: &[u8]) {}
+
+    pub fn remove_code(address: &H160) {}
+
+    pub fn get_code(address: &H160) -> Vec<u8> {
+        [].to_vec() // TODO
+    }
+
+    pub fn set_nonce(address: &H160, nonce: &U256) {}
+
+    pub fn remove_nonce(address: &H160) {}
+
+    pub fn get_nonce(address: &H160) -> U256 {
+        U256::zero() // TODO
+    }
+
+    pub fn set_balance(address: &H160, balance: &U256) {}
+
+    pub fn remove_balance(address: &H160) {}
+
+    pub fn get_balance(address: &H160) -> U256 {
+        U256::zero() // TODO
+    }
+
+    pub fn remove_storage(address: &H160, key: &H256) {}
+
+    pub fn set_storage(address: &H160, key: &H256, value: &H256) {}
+
+    pub fn get_storage(address: &H160, key: &H256) -> H256 {
+        H256::zero() // TODO
+    }
+
+    pub fn is_account_empty(address: &H160) -> bool {
+        true
+    }
+
+    pub fn remove_all_storage(_address: &H160) {}
+
+    pub fn remove_account_if_empty(address: &H160) {}
+
+    pub fn remove_account(address: &H160) {
+        Self::remove_nonce(address);
+        Self::remove_balance(address);
+        Self::remove_code(address);
+        Self::remove_all_storage(address);
+    }
+
+    pub fn deploy_code(&self, input: &[u8]) -> (ExitReason, H160) {
+        (ExitReason::Succeed(ExitSucceed::Stopped), H160::zero()) // TODO
+    }
+
+    pub fn call(&self, input: &[u8]) -> (ExitReason, Vec<u8>) {
+        (ExitReason::Succeed(ExitSucceed::Stopped), [].to_vec()) // TODO
+    }
+
+    pub fn view(&self, args: ViewCallArgs) -> (ExitReason, Vec<u8>) {
+        (ExitReason::Succeed(ExitSucceed::Stopped), [].to_vec()) // TODO
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,9 @@ mod sdk;
 mod contract {
     use crate::engine::Engine;
     use crate::sdk;
-    use crate::types::{near_account_to_evm_address, GetStorageAtArgs, ViewCallArgs};
+    use crate::types::{near_account_to_evm_address, u256_to_arr, GetStorageAtArgs, ViewCallArgs};
     use borsh::BorshDeserialize;
+    use evm::ExitReason;
     use primitive_types::{H160, H256};
 
     const CHAIN_ID: u64 = 1313161556; // FIXME
@@ -41,92 +42,103 @@ mod contract {
 
     #[no_mangle]
     pub extern "C" fn get_version() {
-        sdk::return_output("0.0.0".as_bytes())
+        sdk::return_output(b"0.0.0")
     }
 
     #[no_mangle]
     pub extern "C" fn deploy_code() {
-        let _input = sdk::read_input();
-        let mut _backend = Engine::new(CHAIN_ID, predecessor_address());
-        // TODO
-        sdk::return_output(&[]);
+        let input = sdk::read_input();
+        let mut engine = Engine::new(CHAIN_ID, predecessor_address());
+        let (reason, return_value) = Engine::deploy_code(&mut engine, &input);
+        // TODO: charge for storage
+        process_exit_reason(reason, &return_value.0)
     }
 
     #[no_mangle]
     pub extern "C" fn call() {
-        let _input = sdk::read_input();
-        let mut _backend = Engine::new(CHAIN_ID, predecessor_address());
-        // TODO
-        sdk::return_output(&[]);
+        let input = sdk::read_input();
+        let mut engine = Engine::new(CHAIN_ID, predecessor_address());
+        let (reason, return_value) = Engine::call(&mut engine, &input);
+        // TODO: charge for storage
+        process_exit_reason(reason, &return_value)
     }
 
     #[no_mangle]
     pub extern "C" fn raw_call() {
         let _input = sdk::read_input();
         // TODO
-        sdk::return_output(&[]);
+        sdk::return_output(&[])
     }
 
     #[no_mangle]
     pub extern "C" fn meta_call() {
         let _input = sdk::read_input();
         // TODO
-        sdk::return_output(&[]);
+        sdk::return_output(&[])
     }
 
     #[no_mangle]
     pub extern "C" fn view() {
         let input = sdk::read_input();
         let args = ViewCallArgs::try_from_slice(&input).unwrap();
-        let mut _backend = Engine::new(CHAIN_ID, H160::from_slice(&args.sender));
-        // TODO
-        sdk::return_output(&[]);
+        let mut engine = Engine::new(CHAIN_ID, H160::from_slice(&args.sender));
+        let (reason, return_value) = Engine::view(&mut engine, args);
+        process_exit_reason(reason, &return_value)
     }
 
     #[no_mangle]
     pub extern "C" fn get_code() {
-        let _address = sdk::read_input_arr20();
-        // TODO
-        sdk::return_output(&[]);
+        let address = sdk::read_input_arr20();
+        let code = Engine::get_code(&H160(address));
+        sdk::return_output(&code)
     }
 
     #[no_mangle]
     pub extern "C" fn get_balance() {
-        let _address = sdk::read_input_arr20();
-        // TODO
-        sdk::return_output(&[]);
+        let address = sdk::read_input_arr20();
+        let balance = Engine::get_balance(&H160(address));
+        sdk::return_output(&u256_to_arr(&balance))
     }
 
     #[no_mangle]
     pub extern "C" fn get_nonce() {
-        let _address = sdk::read_input_arr20();
-        // TODO
-        sdk::return_output(&[]);
+        let address = sdk::read_input_arr20();
+        let nonce = Engine::get_nonce(&H160(address));
+        sdk::return_output(&u256_to_arr(&nonce))
     }
 
     #[no_mangle]
     pub extern "C" fn get_storage_at() {
         let input = sdk::read_input();
-        let _args = GetStorageAtArgs::try_from_slice(&input).unwrap();
-        // TODO
-        sdk::return_output(&[]);
+        let args = GetStorageAtArgs::try_from_slice(&input).unwrap();
+        let value = Engine::get_storage(&H160(args.address), &H256(args.key));
+        sdk::return_output(&value.0)
     }
 
     #[no_mangle]
     pub extern "C" fn begin_chain() {
         let _input = sdk::read_input();
         // TODO
-        sdk::return_output(&[]);
+        sdk::return_output(&[])
     }
 
     #[no_mangle]
     pub extern "C" fn begin_block() {
         let _input = sdk::read_input();
         // TODO
-        sdk::return_output(&[]);
+        sdk::return_output(&[])
     }
 
     fn predecessor_address() -> H160 {
         near_account_to_evm_address(&sdk::predecessor_account_id())
+    }
+
+    fn process_exit_reason(reason: ExitReason, return_value: &[u8]) {
+        match reason {
+            ExitReason::Succeed(_) => sdk::return_output(return_value),
+            ExitReason::Revert(_) => sdk::panic_hex(&return_value),
+            ExitReason::Error(error) => sdk::panic_utf8(b"error"), // TODO
+            ExitReason::Fatal(error) => sdk::panic_utf8(b"fatal error"), // TODO
+        }
     }
 }
