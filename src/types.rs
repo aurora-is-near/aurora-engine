78c7c1f3d34b4da028cdf5f59a27bcf184494faf
Arto Bendiken <arto@near.org>
2021-03-12 13:29:40 +0000

    Extend `begin_block` parameters. (#2)

--- src/types.rs
+++ src/types.rs
@@ -18,12 +18,14 @@ pub type RawAddress = [u8; 20];
 pub type RawU256 = [u8; 32];
 pub type RawH256 = [u8; 32];
 
+/// Borsh-encoded parameters for the `call` function.
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct FunctionCallArgs {
     pub contract: RawAddress,
     pub input: Vec<u8>,
 }
 
+/// Borsh-encoded parameters for the `view` function.
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct ViewCallArgs {
     pub sender: RawAddress,
@@ -32,19 +34,24 @@ pub struct ViewCallArgs {
     pub input: Vec<u8>,
 }
 
+/// Borsh-encoded parameters for the `get_storage_at` function.
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct GetStorageAtArgs {
     pub address: RawAddress,
     pub key: RawH256,
 }
 
+/// Borsh-encoded parameters for the `begin_chain` function.
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct BeginChainArgs {
     pub chain_id: RawU256,
 }
 
+/// Borsh-encoded parameters for the `begin_block` function.
 #[derive(BorshSerialize, BorshDeserialize)]
 pub struct BeginBlockArgs {
+    /// The current block's hash (for replayer use).
+    pub hash: RawU256,
     /// The current block's beneficiary address.
     pub coinbase: RawU256,
     /// The current block's timestamp (in seconds since the Unix epoch).
