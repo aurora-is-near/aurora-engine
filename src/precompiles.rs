8ce568b2692fcb6a35ace9e48a09feb1466e0284
Arto Bendiken <arto@near.org>
2021-03-17 18:54:04 +0000

    Fix all Clippy warnings.

--- src/precompiles.rs
+++ src/precompiles.rs
@@ -6,21 +6,24 @@ use std::vec::Vec;
 use evm::{Context, ExitError, ExitSucceed};
 use primitive_types::H160;
 
+type PrecompileResult = Result<(ExitSucceed, Vec<u8>, u64), ExitError>;
+
 #[allow(dead_code)]
 pub fn no_precompiles(
     _address: H160,
     _input: &[u8],
     _target_gas: Option<u64>,
     _context: &Context,
-) -> Option<Result<(ExitSucceed, Vec<u8>, u64), ExitError>> {
-    None
+) -> Option<PrecompileResult> {
+    None // not precompiles supported
 }
 
+#[allow(dead_code)]
 pub fn istanbul_precompiles(
     _address: H160,
     _input: &[u8],
     _target_gas: Option<u64>,
     _context: &Context,
-) -> Option<Result<(ExitSucceed, Vec<u8>, u64), ExitError>> {
+) -> Option<PrecompileResult> {
     None // TODO: implement Istanbul precompiles
 }
