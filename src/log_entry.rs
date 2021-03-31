8fe5e65d450f54eb6b0f91bdb07e27bb220aa91b
Evgeny Ukhanov <mrlsd@ya.ru>
2021-03-31 10:19:21 +0300

    Added LogEntry functionality

---
+++ src/log_entry.rs
@@ -0,0 +1,29 @@
+use alloc::vec::Vec;
+use primitive_types::{H160, H256};
+
+#[derive(Default, Debug, Clone, PartialEq, Eq)]
+pub struct LogEntry {
+    pub address: H160,
+    pub topics: Vec<H256>,
+    pub data: Vec<u8>,
+}
+
+impl rlp::Decodable for LogEntry {
+    fn decode(rlp: &rlp::Rlp) -> Result<Self, rlp::DecoderError> {
+        let result = LogEntry {
+            address: rlp.val_at(0usize)?,
+            topics: rlp.list_at(1usize)?,
+            data: rlp.val_at(2usize)?,
+        };
+        Ok(result)
+    }
+}
+
+impl rlp::Encodable for LogEntry {
+    fn rlp_append(&self, stream: &mut rlp::RlpStream) {
+        stream.begin_list(3usize);
+        stream.append(&self.address);
+        stream.append_list::<H256, _>(&self.topics);
+        stream.append(&self.data);
+    }
+}
