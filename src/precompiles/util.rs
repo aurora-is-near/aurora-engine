310ed1bd63fa23e313d4b6ed02ab7681854c856e
Joshua J. Bouw <dev@joshuajbouw.com>
2021-04-20 19:47:23 +0700

    reduce pad_input method

--- src/precompiles/util.rs
+++ src/precompiles/util.rs
@@ -2,16 +2,8 @@ use crate::prelude::*;
 
 /// Pad the input with a given length, if necessary.
 pub(super) fn pad_input(input: &[u8], len: usize) -> Vec<u8> {
-    let input = if input.len() < len {
-        let mut input = input.to_vec();
-        input.reserve_exact(len);
-        for _ in 0..(128 - input.len()) {
-            input.push(0);
-        }
-        input
-    } else {
-        input.to_vec()
-    };
+    let mut input = input.to_vec();
+    input.resize(len, 0);
 
     input
 }
