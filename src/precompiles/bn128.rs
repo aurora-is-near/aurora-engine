d0c30cca193d0e7a8a2f5c7cc2f1a54a183fcaee
Joshua J. Bouw <dev@joshuajbouw.com>
2021-04-20 23:45:15 +0700

    fix alt_bn_128_sum and test

--- src/precompiles/bn128.rs
+++ src/precompiles/bn128.rs
@@ -4,9 +4,14 @@ use evm::ExitError;
 fn read_point(input: &[u8], pos: usize) -> Result<bn::G1, ExitError> {
     use bn::{AffineG1, Fq, Group, G1};
 
-    let px = Fq::from_slice(&input[pos..(pos + 32)])
+    let mut px_buf = [0u8; 32];
+    px_buf.copy_from_slice(&input[pos..(pos + 32)]);
+    let px = Fq::interpret(&px_buf)
         .map_err(|_e| ExitError::Other(Borrowed("invalid `x` point")))?;
-    let py = Fq::from_slice(&input[(pos + 32)..(pos + 64)])
+
+    let mut py_buf = [0u8; 32];
+    py_buf.copy_from_slice(&input[(pos + 32)..(pos + 64)]);
+    let py = Fq::interpret(&py_buf)
         .map_err(|_e| ExitError::Other(Borrowed("invalid `y` point")))?;
 
     Ok(if px == Fq::zero() && py == bn::Fq::zero() {
@@ -31,15 +36,15 @@ pub(crate) fn alt_bn128_add(input: &[u8], target_gas: Option<u64>) -> Result<Vec
         }
     }
 
-    let input = super::util::pad_input(input, 96);
+    let input = super::util::pad_input(input, 128);
 
     let p1 = read_point(&input, 0)?;
-    let p2 = read_point(&input, 32)?;
+    let p2 = read_point(&input, 64)?;
 
     let mut output = [0u8; 64];
     if let Some(sum) = AffineG1::from_jacobian(p1 + p2) {
         let x = sum.x().into_u256().to_big_endian();
-        let y = sum.x().into_u256().to_big_endian();
+        let y = sum.y().into_u256().to_big_endian();
         output[0..32].copy_from_slice(&x);
         output[32..64].copy_from_slice(&y);
     }
@@ -63,7 +68,9 @@ pub(crate) fn alt_bn128_mul(input: &[u8], target_gas: Option<u64>) -> Result<Vec
     let input = super::util::pad_input(input, 128);
 
     let p = read_point(&input, 0)?;
-    let fr = bn::Fr::from_slice(&input[32..64])
+    let mut fr_buf = [0u8; 32];
+    fr_buf.copy_from_slice(&input[32..64]);
+    let fr = bn::Fr::interpret(&fr_buf)
         .map_err(|_e| ExitError::Other(Borrowed("invalid field element")))?;
 
     let mut output = [0u8; 64];
@@ -103,17 +110,25 @@ pub(crate) fn alt_bn128_pair(input: &[u8], target_gas: Option<u64>) -> Result<Ve
         let mut vals = Vec::with_capacity(elements);
 
         for idx in 0..elements {
-            let ax = Fq::from_slice(&input[(idx * 192)..(idx * 192 + 32)])
+            let mut buf = [0u8; 32];
+
+            buf.copy_from_slice(&input[(idx * 192)..(idx * 192 + 32)]);
+            let ax = Fq::interpret(&buf)
                 .map_err(|_e| ExitError::Other(Borrowed("invalid `a` argument, `x` coordinate")))?;
-            let ay = Fq::from_slice(&input[(idx * 192 + 32)..(idx * 192 + 64)])
+            buf.copy_from_slice(&input[(idx * 192 + 32)..(idx * 192 + 64)]);
+            let ay = Fq::interpret(&buf)
                 .map_err(|_e| ExitError::Other(Borrowed("invalid `a` argument, `y` coordinate")))?;
-            let bay = Fq::from_slice(&input[(idx * 192 + 64)..(idx * 192 + 96)])
+            buf.copy_from_slice(&input[(idx * 192 + 64)..(idx * 192 + 96)]);
+            let bay = Fq::interpret(&buf)
                 .map_err(|_e| ExitError::Other(Borrowed("invalid `a` argument, `x` coordinate")))?;
-            let bax = Fq::from_slice(&input[(idx * 192 + 96)..(idx * 192 + 128)])
+            buf.copy_from_slice(&input[(idx * 192 + 96)..(idx * 192 + 128)]);
+            let bax = Fq::interpret(&buf)
                 .map_err(|_e| ExitError::Other(Borrowed("invalid `a` argument, `x` coordinate")))?;
-            let bby = Fq::from_slice(&input[(idx * 192 + 128)..(idx * 192 + 160)])
+            buf.copy_from_slice(&input[(idx * 192 + 128)..(idx * 192 + 160)]);
+            let bby = Fq::interpret(&buf)
                 .map_err(|_e| ExitError::Other(Borrowed("invalid `a` argument, `x` coordinate")))?;
-            let bbx = Fq::from_slice(&input[(idx * 192 + 160)..(idx * 192 + 192)])
+            buf.copy_from_slice(&input[(idx * 192 + 160)..(idx * 192 + 192)]);
+            let bbx = Fq::interpret(&buf)
                 .map_err(|_e| ExitError::Other(Borrowed("invalid `a` argument, `x` coordinate")))?;
 
             let a = {
@@ -160,6 +175,24 @@ mod tests {
 
     #[test]
     fn test_alt_bn128_add() {
+        let input = hex::decode(
+            "\
+             18b18acfb4c2c30276db5411368e7185b311dd124691610c5d3b74034e093dc9\
+             063c909c4720840cb5134cb9f59fa749755796819658d32efc0d288198f37266\
+             07c2b7f58a84bd6145f00c9c2bc0bb1a187f20ff2c92963a88019e7c6a014eed\
+             06614e20c147e940f2d70da3f74c9a17df361706a4485c742bd6788478fa17d7",
+        )
+        .unwrap();
+        let expected = hex::decode(
+            "\
+            2243525c5efd4b9c3d3c45ac0ca3fe4dd85e830a4ce6b65fa1eeaee202839703\
+            301d1d33be6da8e509df21cc35964723180eed7532537db9ae5e7d48f195c915",
+        )
+        .unwrap();
+
+        let res = alt_bn128_add(&input, None).unwrap();
+        assert_eq!(res, expected);
+
         // zero sum test
         let input = hex::decode(
             "\
