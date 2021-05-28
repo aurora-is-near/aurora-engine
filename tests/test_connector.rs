8890f91ef3947ac8bb73fad34f76feac80a79224
Septen <gammerxpower@gmail.com>
2021-05-28 03:02:43 +0300

    Eth-connector tests:improve checks for failure msg.
    * Usually the converted to string has either of following two messages formats:
      "Action #0: Smart contract panicked: ERR_MSG [src/some_file.rs:LINE_NUMBER:COLUMN_NUMBER]"
      "right: 'MISMATCHED_DATA': ERR_MSG [src/some_file.rs:LINE_NUMBER:COLUMN_NUMBER]"
      So the ": ERR_MSG [" pattern should catch all invariants of error,
      even if one of the errors message is a subset of another one
      (e.g. `ERR_MSG_FAILED` is a subset of `ERR_MSG_FAILED_FOO`).

--- tests/test_connector.rs
+++ tests/test_connector.rs
@@ -1136,9 +1136,17 @@ fn assert_execution_status_failure(
     err_msg: &str,
     panic_msg: &str,
 ) {
+    // Usually the converted to string has either of following two messages formats:
+    // "Action #0: Smart contract panicked: ERR_MSG [src/some_file.rs:LINE_NUMBER:COLUMN_NUMBER]"
+    // "right: 'MISMATCHED_DATA': ERR_MSG [src/some_file.rs:LINE_NUMBER:COLUMN_NUMBER]"
+    // So the ": ERR_MSG [" pattern should catch all invariants of error, even if one of the errors
+    // message is a subset of another one (e.g. `ERR_MSG_FAILED` is a subset of `ERR_MSG_FAILED_FOO`)
+    let expected_err_msg_pattern = format!(": {} [", err_msg);
+
     match execution_status {
         ExecutionStatus::Failure(err) => {
-            assert!(err.to_string().contains(err_msg));
+            println!("Error: {}", err);
+            assert!(err.to_string().contains(&expected_err_msg_pattern));
         }
         _ => panic!("{}", panic_msg),
     }
