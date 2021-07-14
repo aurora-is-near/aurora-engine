5e5c8e0c5d97b5ee4a83d14d7b40f5abd45a8f81
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2021-07-14 18:40:15 +0200

    feat: storage layout for EvmErc20.sol (#178)
    * feat: storage layout for EvmErc20.sol
    
    * fix: lint errors

--- etc/eth-contracts/hardhat.config.js
+++ etc/eth-contracts/hardhat.config.js
@@ -1,16 +1,23 @@
 require('@nomiclabs/hardhat-waffle');
 require('solidity-coverage');
+require('./tasks/storage');
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+    newStorageLayoutPath: './storageLayout',
     solidity: {
         version: '0.8.3',
         settings: {
             optimizer: {
                 enabled: true,
                 runs: 1000,
+            },
+            outputSelection: {
+                '*': {
+                    '*': ['storageLayout'],
+                },
             },
         },
     },
