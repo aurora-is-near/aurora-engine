3b9119adacbc2ff71d580f6aa0b850de66431987
Michael Birch <michael.birch@aurora.dev>
2021-08-12 17:34:42 +0000

    feature(map_into_keys_values)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(array_methods)]
+#![feature(map_into_keys_values)]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(not(feature = "std"), feature(alloc_error_handler))]
 #![cfg_attr(feature = "log", feature(panic_info_message))]
