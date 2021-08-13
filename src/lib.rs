fb65666d2c0515890ccde8a2cfe901c1784a493f
Joshua J. Bouw <joshua@aurora.dev>
2021-08-13 16:10:25 +0700

    Remove map values feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 #![feature(array_methods)]
-#![feature(map_into_keys_values)]
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(not(feature = "std"), feature(alloc_error_handler))]
 #![cfg_attr(feature = "log", feature(panic_info_message))]
