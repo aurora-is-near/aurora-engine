ee6c2648629d12c5241d22601212d6e233192f3c
Evgeny Ukhanov <mrlsd@ya.ru>
2021-08-19 10:17:18 +0300

    Fix tests

--- engige/src/test_utils/mod.rs
+++ engige/src/test_utils/mod.rs
@@ -305,11 +305,11 @@ impl Default for AuroraRunner {
     fn default() -> Self {
         let aurora_account_id = "aurora".to_string();
         let evm_wasm_bytes = if cfg!(feature = "mainnet-test") {
-            std::fs::read("mainnet-test.wasm").unwrap()
+            std::fs::read("../mainnet-test.wasm").unwrap()
         } else if cfg!(feature = "testnet-test") {
-            std::fs::read("testnet-test.wasm").unwrap()
+            std::fs::read("../testnet-test.wasm").unwrap()
         } else {
-            std::fs::read("betanet-test.wasm").unwrap()
+            std::fs::read("../betanet-test.wasm").unwrap()
         };
 
         Self {
