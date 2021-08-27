13bf0b9e8b8feae4ef0df3d9978a303664c77fa3
Michael Birch <michael.birch@aurora.dev>
2021-08-27 15:39:20 +0200

    benches module for NFT pagination test

--- src/benches/mod.rs
+++ src/benches/mod.rs
@@ -4,6 +4,7 @@ mod eth_deploy_code;
 mod eth_erc20;
 mod eth_standard_precompiles;
 mod eth_transfer;
+mod nft_pagination;
 
 // We don't want to run in CI, so ignore. To run locally use `cargo test --release -- --ignored`
 #[test]
