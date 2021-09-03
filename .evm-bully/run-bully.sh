f56172a8a7da3e65ea296dc81e5caf9a410723b5
Dmitry Strokov <dmitry.strokov@aurora.dev>
2021-09-03 19:06:59 +0700

    Add test-results checking

--- .evm-bully/run-bully.sh
+++ .evm-bully/run-bully.sh
@@ -1,4 +1,7 @@
-#!/bin/sh -e
+#!/bin/bash -e
+
+
+# Validating arguments
 
 if [ $# -ne 1 ]
 then
@@ -6,13 +9,40 @@ then
   exit 1
 fi
 
+
+# Determining paths
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 CONTRACT_PATH=$SCRIPT_DIR/../mainnet-release.wasm
+STATE_PATH=$SCRIPT_DIR/state.json
 
-export NEAR_ENV=local
+
+# Running bully
 
 cd $SCRIPT_DIR/workdir/evm-bully
+rm -f *.tar.gz
+
+export NEAR_ENV=local
 ./evm-bully -v replay \
     -initial-balance 1000 \
     -keyPath $HOME/.near/local/validator_key.json \
     -autobreak -setup -skip $1 -contract $CONTRACT_PATH
+
+
+# Examining results
+
+TESTNET='goerli' # HARDCODED temporary
+ARTIFACT=$(ls | grep tar | grep $TESTNET | head -1)
+EXPECTED_ARTIFACT=$(jq -r '.progress.goerli' $STATE_PATH)
+
+if [[ $ARTIFACT == "" ]]; then
+    echo "Bully results are empty"
+    exit 1
+fi
+
+if [[ $ARTIFACT == $EXPECTED_ARTIFACT ]]; then
+    echo "Success"
+else
+    echo "Expected artifact was $EXPECTED_ARTIFACT, but $ARTIFACT was found"
+    exit 1
+fi
