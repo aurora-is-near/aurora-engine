f51395f5f4f3bb72ea2a5166d0105eb6a8edca5a
Evgeny Ukhanov <mrlsd@ya.ru>
2021-09-10 17:20:41 +0300

    Remove precompiles module from engine

--- engine/src/precompiles/utils.rs
+++
@@ -1,9 +0,0 @@
-use evm::Context;
-
-pub fn new_context() -> Context {
-    Context {
-        address: Default::default(),
-        caller: Default::default(),
-        apparent_value: Default::default(),
-    }
-}
