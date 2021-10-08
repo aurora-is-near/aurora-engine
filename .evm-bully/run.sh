7d2ba20c7925f63b28910e05b579b4dc845257ca
Dmitry Strokov <dmitry.strokov@aurora.dev>
2021-10-08 08:59:00 +0300

    Fix: pass testnet name to run script

--- .evm-bully/run.sh
+++ .evm-bully/run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -e
 
+if [ $# -ne 1 ]
+then
+  echo "Usage: $0 [goerli | ropsten | rinkeby]" >&2
+  exit 1
+fi
+
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 $CURRENT_DIR/scripts/make_aurora_cli.sh
 $CURRENT_DIR/scripts/make_aurora_engine.sh
 $CURRENT_DIR/scripts/make_evm_bully.sh
 $CURRENT_DIR/scripts/make_nearcore.sh
-$CURRENT_DIR/scripts/run_bully.sh
+$CURRENT_DIR/scripts/run_bully.sh $1
