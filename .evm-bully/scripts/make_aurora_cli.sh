2f1ee6d92d0d9188befddc235a0e09d245b15917
Dmitry Strokov <dmitry.strokov@aurora.dev>
2021-10-08 08:53:33 +0300

    Fix typo

--- .evm-bully/scripts/make_aurora_cli.sh
+++ .evm-bully/scripts/make_aurora_cli.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 source $SCRIPT_DIR/common.sh
 
 
-if [[ ! -z $AURORA_CLI_PREVENT_CHECKOUT ]]; then
+if [[ -z $AURORA_CLI_PREVENT_CHECKOUT ]]; then
     echo "aurora-cli: checkouting repo..."
     checkout_repo $AURORA_CLI_REPO_PATH https://github.com/aurora-is-near/aurora-cli.git $AURORA_CLI_VERSION
 fi
