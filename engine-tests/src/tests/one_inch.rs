d3a9393114df550c5a21529e83f2ad0f15010bbc
Evgeny Ukhanov <evgeny@aurora.dev>
2022-01-04 16:42:31 +0200

    Changed gasfor test_1_inch_limit_order_deploy

--- engine-tests/src/tests/one_inch.rs
+++ engine-tests/src/tests/one_inch.rs
@@ -100,8 +100,8 @@ fn test_1_inch_limit_order_deploy() {
 
     // more than 3.5 million Ethereum gas used
     assert!(result.gas_used > 3_500_000);
-    // less than 14 NEAR Tgas used
-    assert_gas_bound(profile.all_gas(), 14);
+    // less than 15 NEAR Tgas used
+    assert_gas_bound(profile.all_gas(), 15);
     // at least 45% of which is from wasm execution
     let wasm_fraction = 100 * profile.wasm_gas() / profile.all_gas();
     assert!(
