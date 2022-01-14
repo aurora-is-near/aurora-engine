1bcd44423dfbcfa34fb9d7e1f4565033ebe1225c
Michael Birch <michael.birch@aurora.dev>
2022-01-14 21:44:57 +0100

    Feat(tests): Uniswap multi-hop swap benchmark (#415)

--- engine-tests/src/test_utils/uniswap.rs
+++ engine-tests/src/test_utils/uniswap.rs
@@ -303,6 +303,16 @@ pub struct ExactOutputSingleParams {
     pub price_limit: U256,
 }
 
+pub struct ExactInputParams {
+    pub token_in: Address,
+    // Vec of poolFee + tokenAddress
+    pub path: Vec<(u64, Address)>,
+    pub recipient: Address,
+    pub deadline: U256,
+    pub amount_in: U256,
+    pub amount_out_min: U256,
+}
+
 impl SwapRouter {
     pub fn exact_output_single(
         &self,
@@ -323,6 +333,42 @@ impl SwapRouter {
                 ethabi::Token::Uint(params.amount_out),
                 ethabi::Token::Uint(params.amount_in_max),
                 ethabi::Token::Uint(params.price_limit),
+            ])])
+            .unwrap();
+
+        TransactionLegacy {
+            nonce,
+            gas_price: Default::default(),
+            gas_limit: u64::MAX.into(),
+            to: Some(self.0.address),
+            value: Default::default(),
+            data,
+        }
+    }
+
+    pub fn exact_input(&self, params: ExactInputParams, nonce: U256) -> TransactionLegacy {
+        let path: Vec<u8> = {
+            // The encoding here is 32-byte address, then 3-byte (24-bit) fee, alternating
+            let mut result = Vec::with_capacity(32 + 35 * params.path.len());
+            result.extend_from_slice(params.token_in.as_bytes());
+            for (fee, token) in params.path.iter() {
+                let fee_bytes = fee.to_be_bytes();
+                result.extend_from_slice(&fee_bytes[5..8]);
+                result.extend_from_slice(token.as_bytes());
+            }
+            result
+        };
+        let data = self
+            .0
+            .abi
+            .function("exactInput")
+            .unwrap()
+            .encode_input(&[ethabi::Token::Tuple(vec![
+                ethabi::Token::Bytes(path),
+                ethabi::Token::Address(params.recipient.raw()),
+                ethabi::Token::Uint(params.deadline),
+                ethabi::Token::Uint(params.amount_in),
+                ethabi::Token::Uint(params.amount_out_min),
             ])])
             .unwrap();
 
