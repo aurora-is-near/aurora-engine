6ee8bde9dc86a8d43d73318cb3046b1e6ba12b1e
Michael Birch <michael.birch@aurora.dev>
2022-02-15 16:20:00 +0100

    Update gas bounds since new Sputnik is more efficient

--- engine-tests/src/tests/sanity.rs
+++ engine-tests/src/tests/sanity.rs
@@ -92,8 +92,8 @@ fn test_deploy_largest_contract() {
         result.gas_used,
     );
 
-    // Less than 18 NEAR Tgas
-    test_utils::assert_gas_bound(profile.all_gas(), 18);
+    // Less than 14 NEAR Tgas
+    test_utils::assert_gas_bound(profile.all_gas(), 14);
 }
 
 #[test]
