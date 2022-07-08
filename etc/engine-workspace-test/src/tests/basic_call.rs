916a874b7ac626c6162a45fe4d95fc1b39b5b84b
Hyungsuk Kang <hyungsuk@aurora.dev>
2022-07-08 20:41:09 +0900

    add starter for workspace test
    Signed-off-by: Hyungsuk Kang <hskang9@gmail.com>

---
+++ etc/engine-workspace-test/src/tests/basic_call.rs
@@ -0,0 +1,38 @@
+use serde_json::json;
+
+use workspaces::prelude::*;
+use workspaces::Contract;
+use workspaces::result::CallExecutionDetails;
+
+const AURORA_WASM_FILEPATH: &str = "../../mainnet-release.wasm";
+
+
+async fn start_engine() -> anyhow::Result<(Worker, Contract)> {
+    let worker = workspaces::sandbox().await?;
+    let wasm = std::fs::read(AURORA_WASM_FILEPATH)?;
+    let contract = worker.dev_deploy(&wasm).await?;
+    return Ok((worker, contract));
+}
+
+#[tokio::test]
+async fn get_version() -> anyhow::Result<()> {
+    //let worker = workspaces::sandbox().await?;
+    //let wasm = std::fs::read(AURORA_WASM_FILEPATH)?;
+    let (worker, contract) = start_engine().await?;
+
+    let outcome = contract
+        .call(&worker, "get_version")
+        .args_json(json!({}))?
+        .transact()
+        .await?;
+
+    println!("get_version outcome: {:#?}", outcome);
+
+    println!("Dev Account ID: {}", contract.id());
+    Ok(())
+}
+
+#[tokio::test]
+async fn eth_call() -> anyhow::Result<()> {
+    Ok(())
+}
