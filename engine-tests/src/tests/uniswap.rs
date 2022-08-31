1437b5a455c98bfd1851bb3f374f3b86d405e71d
Michael Birch <michael.birch@aurora.dev>
2022-08-31 16:26:13 +0200

    Fix some gas costs

--- engine-tests/src/tests/uniswap.rs
+++ engine-tests/src/tests/uniswap.rs
@@ -50,7 +50,7 @@ fn test_uniswap_exact_output() {
 
     let (_result, profile) =
         context.add_equal_liquidity(LIQUIDITY_AMOUNT.into(), &token_a, &token_b);
-    test_utils::assert_gas_bound(profile.all_gas(), 33);
+    test_utils::assert_gas_bound(profile.all_gas(), 31);
     let wasm_fraction = 100 * profile.wasm_gas() / profile.all_gas();
     assert!(
         40 <= wasm_fraction && wasm_fraction <= 50,
@@ -60,7 +60,7 @@ fn test_uniswap_exact_output() {
 
     let (_amount_in, profile) =
         context.exact_output_single(&token_a, &token_b, OUTPUT_AMOUNT.into());
-    test_utils::assert_gas_bound(profile.all_gas(), 18);
+    test_utils::assert_gas_bound(profile.all_gas(), 17);
     let wasm_fraction = 100 * profile.wasm_gas() / profile.all_gas();
     assert!(
         45 <= wasm_fraction && wasm_fraction <= 55,
