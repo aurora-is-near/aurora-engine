931de6b2e2b630488549c86bd98e19297b5617f4
hskang9 <hyungsuk@aurora.dev>
2022-12-22 02:35:06 +0400

    update directories

--- engine-tests/src/tests/ethereum_tests/general_state_tests/mod.rs
+++ engine-tests/src/tests/ethereum_tests/general_state_tests/mod.rs
@@ -81,6 +81,9 @@ fn run(path: String, name: String) {
 pub fn run_dir(dir: &str) {
     // get file names in the directory
     let paths = std::fs::read_dir(dir).unwrap();
+    if path.is_none() {
+        println!("path {} does not exist", paths);
+    }
     for path in paths {
         let path = path.unwrap().path();
         let path_str = path.to_str().unwrap().to_string();
@@ -111,7 +114,7 @@ pub fn run_dir(dir: &str) {
 #[test]
 pub fn test_add() {
     run(
-        "../etc/eth-json-test/res/tests/GeneralStateTests/VMTests/vmArithmeticTest/add.json"
+        "../../etc/eth-json-test/res/tests/GeneralStateTests/VMTests/vmArithmeticTest/add.json"
             .to_string(),
         "add".to_string(),
     )
@@ -121,293 +124,294 @@ pub fn test_add() {
 pub fn test_state_zero_one_balance() {
     let cwd = std::env::current_dir().unwrap();
     println!("Current directory is {}", cwd.display());
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stZeroOneBalance");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stZeroOneBalance");
 }
 
 #[test]
 pub fn test_state_attack_test() {
     let cwd = std::env::current_dir().unwrap();
     println!("Current directory is {}", cwd.display());
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stAttackTest");
+
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stAttackTest");
 }
 
 #[test]
 pub fn test_state_bad_opcode() {
     let cwd = std::env::current_dir().unwrap();
     println!("Current directory is {}", cwd.display());
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stBadOpcode");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stBadOpcode");
 }
 
 #[test]
 pub fn test_state_bugs() {
     let cwd = std::env::current_dir().unwrap();
     println!("Current directory is {}", cwd.display());
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stBugs");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stBugs");
 }
 
 #[test]
 pub fn test_state_call_codes() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stCallCodes");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stCallCodes");
 }
 
 #[test]
 pub fn test_state_call_create_call_code() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stCallCreateCallCodeTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stCallCreateCallCodeTest");
 }
 
 #[test]
 pub fn test_state_call_delegatecall_codecall() {
     run_dir(
-        "../etc/eth-json-test/res/tests/GeneralStateTests/stCallDelegateCodesCallCodeHomestead",
+        "../../etc/eth-json-test/res/tests/GeneralStateTests/stCallDelegateCodesCallCodeHomestead",
     );
 }
 
 #[test]
 pub fn test_state_call_delegatecall_homestead() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stCallDelegateCodesHomestead");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stCallDelegateCodesHomestead");
 }
 
 #[test]
 pub fn test_state_chain_id() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stChainId");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stChainId");
 }
 
 #[test]
 pub fn test_state_code_copy() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stCodeCopyTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stCodeCopyTest");
 }
 
 #[test]
 pub fn test_state_code_size_limit() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stCodeSizeLimit");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stCodeSizeLimit");
 }
 
 #[test]
 pub fn test_state_create2() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stCreate2");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stCreate2");
 }
 
 #[test]
 pub fn test_state_create() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stCreateTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stCreateTest");
 }
 
 #[test]
 pub fn test_state_delegatecall() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stDelegatecallTestHomestead");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stDelegatecallTestHomestead");
 }
 
 #[test]
 pub fn test_state_eip150_single_code_gas_prices() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stEIP150singleCodeGasPrices");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stEIP150singleCodeGasPrices");
 }
 
 #[test]
 pub fn test_state_eip150_specific() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stEIP150Specific");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stEIP150Specific");
 }
 
 #[test]
 pub fn test_state_eip158_specific() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stEIP158");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stEIP158");
 }
 
 #[test]
 pub fn test_state_eip1559() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stEIP1559");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stEIP1559");
 }
 
 #[test]
 pub fn test_state_eip2930() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stEIP2930");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stEIP2930");
 }
 
 #[test]
 pub fn test_state_eip3607() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stEIP3607");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stEIP3607");
 }
 
 #[test]
 pub fn test_state_example() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stExample");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stExample");
 }
 
 #[test]
 pub fn test_state_ext_code_hash() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stExtCodeHash");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stExtCodeHash");
 }
 
 #[test]
 pub fn test_state_homestead_specific() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stHomesteadSpecific");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stHomesteadSpecific");
 }
 
 #[test]
 
 pub fn test_state_init_code() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stInitCodeTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stInitCodeTest");
 }
 
 #[test]
 pub fn test_state_log() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stLogTests");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stLogTests");
 }
 
 #[test]
 pub fn test_state_mem_expanding_eip150_calls() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stMemExpandingEIP150Calls");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stMemExpandingEIP150Calls");
 }
 
 #[test]
 pub fn test_state_memory_stress() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stMemoryStressTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stMemoryStressTest");
 }
 
 #[test]
 pub fn test_state_memory() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stMemoryTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stMemoryTest");
 }
 
 #[test]
 pub fn test_state_non_zero_calls() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stNonZeroCallsTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stNonZeroCallsTest");
 }
 
 #[test]
 pub fn test_state_precompiles() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stPreCompiledContracts");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stPreCompiledContracts");
 }
 
 #[test]
 pub fn test_state_precompiles2() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stPreCompiledContracts2");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stPreCompiledContracts2");
 }
 
 #[test]
 pub fn test_state_quadratic_complexity() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stQuadraticComplexityTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stQuadraticComplexityTest");
 }
 
 #[test]
 pub fn test_state_random() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stRandom");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stRandom");
 }
 
 #[test]
 pub fn test_state_random2() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stRandom2");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stRandom2");
 }
 
 #[test]
 pub fn test_state_recursive_create() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stRecursiveCreate");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stRecursiveCreate");
 }
 
 #[test]
 pub fn test_state_refund() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stRefundTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stRefundTest");
 }
 
 #[test]
 pub fn test_state_return_data() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stReturnDataTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stReturnDataTest");
 }
 
 #[test]
 pub fn test_state_revert() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stRevertTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stRevertTest");
 }
 
 #[test]
 pub fn test_state_self_balance() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stSelfBalance");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stSelfBalance");
 }
 
 #[test]
 pub fn test_state_shift() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stShift");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stShift");
 }
 
 #[test]
 pub fn test_state_sload() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stLoadTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stLoadTest");
 }
 
 #[test]
 pub fn test_state_solidity() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stSolidityTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stSolidityTest");
 }
 
 #[test]
 pub fn test_state_special() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stSpecialTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stSpecialTest");
 }
 
 #[test]
 pub fn test_state_store() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stSStoreTests");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stSStoreTests");
 }
 
 #[test]
 pub fn test_state_stack() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stStackTests");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stStackTests");
 }
 
 #[test]
 pub fn test_state_static() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stStaticCall");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stStaticCall");
 }
 
 #[test]
 pub fn test_static_flag_enabled() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stStaticFlagEnabled");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stStaticFlagEnabled");
 }
 
 #[test]
 pub fn test_state_system_operations() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stSystemOperationsTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stSystemOperationsTest");
 }
 
 #[test]
 pub fn test_state_time_consuming() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stTimeConsuming");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stTimeConsuming");
 }
 
 #[test]
 pub fn test_state_transaction() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stTransactionTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stTransactionTest");
 }
 
 #[test]
 pub fn test_state_wallet() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stWalletTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stWalletTest");
 }
 
 #[test]
 pub fn test_state_zero_calls_revert() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stZeroCallsRevert");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stZeroCallsRevert");
 }
 
 #[test]
 pub fn test_state_zero_calls() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stZeroCallsTest");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stZeroCallsTest");
 }
 
 #[test]
 pub fn test_state_zero_knowledge() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stZeroKnowledge");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stZeroKnowledge");
 }
 
 #[test]
 pub fn test_state_zero_knowledge2() {
-    run_dir("../etc/eth-json-test/res/tests/GeneralStateTests/stZeroKnowledge2");
+    run_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/stZeroKnowledge2");
 }
 
 #[test]
 pub fn test_state() {
     let cwd = std::env::current_dir().unwrap();
     println!("Current directory is {}", cwd.display());
-    let paths = std::fs::read_dir("../etc/eth-json-test/res/tests/GeneralStateTests").unwrap();
+    let paths = std::fs::read_dir("../../etc/eth-json-test/res/tests/GeneralStateTests").unwrap();
     for path in paths {
         let path = path.unwrap().path();
         let path_str = path.to_str().unwrap().to_string();
@@ -421,38 +425,38 @@ pub fn test_state() {
 
 #[test]
 pub fn test_vm_arithmetic() {
-    run_dir("../etc/eth-json-test/res/tests/VMTests/vmArithmeticTest");
+    run_dir("../../etc/eth-json-test/res/tests/VMTests/vmArithmeticTest");
 }
 
 #[test]
 pub fn test_vm_bitwise_logic_operation() {
-    run_dir("../etc/eth-json-test/res/tests/VMTests/vmBitwiseLogicOperation");
+    run_dir("../../etc/eth-json-test/res/tests/VMTests/vmBitwiseLogicOperation");
 }
 
 #[test]
 pub fn test_vm_io_and_flow_operations() {
-    run_dir("../etc/eth-json-test/res/tests/VMTests/vmIOandFlowOperations");
+    run_dir("../../etc/eth-json-test/res/tests/VMTests/vmIOandFlowOperations");
 }
 
 #[test]
 pub fn test_vm_log() {
-    run_dir("../etc/eth-json-test/res/tests/VMTests/vmLogTest");
+    run_dir("../../etc/eth-json-test/res/tests/VMTests/vmLogTest");
 }
 
 #[test]
 pub fn test_vm_performance() {
-    run_dir("../etc/eth-json-test/res/tests/VMTests/vmPerformance");
+    run_dir("../../etc/eth-json-test/res/tests/VMTests/vmPerformance");
 }
 
 #[test]
 pub fn test_vm_tests() {
-    run_dir("../etc/eth-json-test/res/tests/VMTests/vmTests");
+    run_dir("../../etc/eth-json-test/res/tests/VMTests/vmTests");
 }
 
 #[test]
 pub fn test_vm() {
     let paths =
-        std::fs::read_dir("../etc/eth-json-test/res/tests/GeneralStateTests/VMTests").unwrap();
+        std::fs::read_dir("../../etc/eth-json-test/res/tests/GeneralStateTests/VMTests").unwrap();
     for path in paths {
         let path = path.unwrap().path();
         let path_str = path.to_str().unwrap().to_string();
