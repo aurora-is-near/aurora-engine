b1aab2184267630dd313b6e22171b426f1233078
hskang9 <hyungsuk@aurora.dev>
2023-02-16 02:31:50 +0400

    remove warnings

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -94,14 +94,12 @@ mod contract {
         near_account_to_evm_address, SdkExpect, SdkProcess, SdkUnwrap,
     };
     use crate::prelude::storage::{bytes_to_key, KeyPrefix};
-    use crate::prelude::String;
     use crate::prelude::{sdk, u256_to_arr, Address, PromiseResult, Yocto, ERR_FAILED_PARSE, H256};
     use crate::{errors, pausables, state};
     use aurora_engine_sdk::env::Env;
     use aurora_engine_sdk::io::{StorageIntermediate, IO};
     use aurora_engine_sdk::near_runtime::{Runtime, ViewEnv};
     use aurora_engine_sdk::promise::PromiseHandler;
-    use core::str::FromStr;
 
     #[cfg(feature = "integration-test")]
     use crate::prelude::NearGas;
