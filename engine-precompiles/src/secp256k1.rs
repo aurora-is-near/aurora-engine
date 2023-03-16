471e21dae6287974d5640b5bc4420254a74954ac
Ahmed Ali <ahmedaabdulwahed@gmail.com>
2023-03-16 13:17:07 +0100

    Update engine-precompiles/src/secp256k1.rs
    Co-authored-by: Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>

--- engine-precompiles/src/secp256k1.rs
+++ engine-precompiles/src/secp256k1.rs
@@ -120,7 +120,7 @@ mod tests {
     use crate::utils::new_context;
 
     fn ecverify(hash: H256, signature: &[u8], signer: Address) -> bool {
-        matches!(ecrecover(hash, signature[0..65].try_into().unwrap()), Ok(s) if s == signer)
+        matches!(ecrecover(hash, signature[0..super::consts::SIGNATURE_LENGTH].try_into().unwrap()), Ok(s) if s == signer)
     }
 
     #[test]
