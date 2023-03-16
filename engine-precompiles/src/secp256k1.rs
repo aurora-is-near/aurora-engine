801ef739167c2476d04799b77db24ed0815f83bf
ahmed <ahmed.ali@aurora.dev>
2023-03-16 12:36:00 +0100

    chore: update tests

--- engine-precompiles/src/secp256k1.rs
+++ engine-precompiles/src/secp256k1.rs
@@ -249,4 +249,17 @@ mod tests {
             .output;
         assert_eq!(res, expected);
     }
+
+    #[test]
+    fn test_extra_input_length() {
+        let input = hex::decode("18c547e4f7b0f325ad1e56f57e26c745b09a3e503d86e00e5255ff7f715d3d1c000000000000000000000000000000000000000000000000000000000000001c73b1693892219d736caba55bdb67216e485557ea6b6af75f37096c9aa6a5a75feeb940b1d03b21e36b0e47e79769f095fe2ab855bd91e3a38756b7d75a9c4549aabbccddeeff").unwrap();
+        let expected =
+            hex::decode("000000000000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b")
+                .unwrap();
+        let res = ECRecover
+            .run(&input, Some(EthGas::new(3_000)), &new_context(), false)
+            .unwrap()
+            .output;
+        assert_eq!(res, expected);
+    }
 }
