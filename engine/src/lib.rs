82e7c5efed9c785cfcbc57d60ebc3dfa20fb7909
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2023-08-03 13:02:33 +0200

    chore: add contract running check for key management function (#812)
    ## Description
    
    The PR adds the contract status check for key management functions.

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -618,6 +618,7 @@ mod contract {
         let mut io = Runtime;
         let mut state = state::get_state(&io).sdk_unwrap();
 
+        require_running(&state);
         require_owner_only(&state, &io.predecessor_account_id());
 
         let key_manager =
@@ -638,6 +639,8 @@ mod contract {
     pub extern "C" fn add_relayer_key() {
         let mut io = Runtime;
         let state = state::get_state(&io).sdk_unwrap();
+
+        require_running(&state);
         require_key_manager_only(&state, &io.predecessor_account_id());
 
         let public_key = serde_json::from_slice::<RelayerKeyArgs>(&io.read_input().to_vec())
@@ -674,6 +677,8 @@ mod contract {
     pub extern "C" fn remove_relayer_key() {
         let mut io = Runtime;
         let state = state::get_state(&io).sdk_unwrap();
+
+        require_running(&state);
         require_key_manager_only(&state, &io.predecessor_account_id());
 
         let args: RelayerKeyArgs = serde_json::from_slice(&io.read_input().to_vec())
