9979779be8c8eb5fdfa4c015e58d50921b71a29c
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2023-10-12 13:01:45 +0100

    ci: use sudo for apt-get

--- scripts/ci/build_rocksdb.sh
+++ scripts/ci/build_rocksdb.sh
@@ -5,7 +5,7 @@ INSTALL_PATH=$HOME/.rocksdb
 LIB_PATH=$INSTALL_PATH/lib/librocksdb.a
 
 if [[ ! -f $LIB_PATH ]]; then
-  apt-get -y install cmake libgflags-dev libsnappy-dev libbz2-dev liblz4-dev libzstd-dev
+  sudo apt-get -y install build-essential cmake libgflags-dev libsnappy-dev libbz2-dev liblz4-dev libzstd-dev
   git clone --branch $ROCKSDB_VER https://github.com/facebook/rocksdb
   cd rocksdb && mkdir build && cd build || exit
   cmake -DCMAKE_BUILD_TYPE=Release -DWITH_ZLIB=1 -DWITH_SNAPPY=1 -DWITH_LZ4=1 -DWITH_ZSTD=1 -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH ..
