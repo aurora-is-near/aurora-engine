fb904e9848f78ff09f024f24108f227a72dfd1e0
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2023-10-12 12:46:12 +0100

    ci: use github heave runners for tests

--- scripts/ci/build_rocksdb.sh
+++ scripts/ci/build_rocksdb.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 ROCKSDB_VER=v8.1.1
-INSTALL_PATH=/root/rocksdb
+INSTALL_PATH=$HOME/.rocksdb
 LIB_PATH=$INSTALL_PATH/lib/librocksdb.a
 
 if [[ ! -f $LIB_PATH ]]; then
   apt -y install cmake libgflags-dev libsnappy-dev libbz2-dev liblz4-dev libzstd-dev
   git clone --branch $ROCKSDB_VER https://github.com/facebook/rocksdb
-  cd rocksdb && mkdir build && cd build
+  cd rocksdb && mkdir build && cd build || exit
   cmake -DCMAKE_BUILD_TYPE=Release -DWITH_ZLIB=1 -DWITH_SNAPPY=1 -DWITH_LZ4=1 -DWITH_ZSTD=1 -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH ..
-  make -j32 install
-  cache-util save rocksdb:$INSTALL_PATH
-  cd $HOME
+  make -j 64 install
+  cd "$HOME" || exit
 fi
