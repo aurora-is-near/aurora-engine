e47d69917911596338e2b32c81ca990dafa22a19
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2024-01-02 13:20:44 +0000

    chore: make clippy happy

--- engine-sdk/src/prelude.rs
+++ engine-sdk/src/prelude.rs
@@ -1,2 +1,4 @@
 pub use aurora_engine_types::types::{Address, NearGas, STORAGE_PRICE_PER_BYTE};
-pub use aurora_engine_types::{vec, Vec, H256, U256};
+#[cfg(feature = "contract")]
+pub use aurora_engine_types::U256;
+pub use aurora_engine_types::{vec, Vec, H256};
