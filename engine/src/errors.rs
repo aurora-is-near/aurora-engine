0ed0a8e88a6a12ada1ae62c4e0768b924574f926
Karim <karim@aurora.dev>
2024-04-04 20:17:36 +0100

    feat: pause FT transfers (#922)
    <!--
    Thanks for submitting a pull request! Here are some helpful tips:
    
    * Always create branches on and target the `develop` branch.
    * Run all the tests locally and ensure that they are passing.
    * Run `make format` to ensure that the code is formatted.
    * Run `make check` to ensure that all checks passed successfully.
    * Small commits and contributions that attempt one single goal is
    preferable.
    * If the idea changes or adds anything functional which will affect
    users, an
    AIP discussion is required first on the Aurora forum: 
    
    https://forum.aurora.dev/discussions/AIPs%20(Aurora%20Improvement%20Proposals).
    * Avoid breaking the public API (namely in engine/src/lib.rs) unless
    required.
    * If your PR is a WIP, ensure that you enable "draft" mode.
    * Your first PRs won't use the CI automatically unless a maintainer
    starts.
    If this is not your first PR, please do NOT abuse the CI resources.
    
    Checklist:
    - [ ] I have performed a self-review of my code
    - [ ] I have documented my code, particularly in the hard-to-understand
    areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] I have added tests to prove my fix or new feature is effective and
    works
    - [ ] Any dependent changes have been merged
    - [ ] The PR is targeting the `develop` branch and not `master`
    - [ ] I have pre-squashed my commits into a single commit and rebased.
    -->
    
    ## Description
    Add the possibility of pausing the ft transfers for the internal
    eth-connector; this is needed to avoid pausing the whole Aurora engine
    during migration.
    A new flag, `PAUSE_FT`, has been added that pauses all the public
    methods that could change the internal balances of the eth-connector
    token [`ft_transfer`, `ft_transfer_call`, `storage_deposit`,
    `storage_unregister`].
    <!-- 
    Provide a general summary of your changes. A clear overview along with
    an
    in-depth explanation is beneficial.
    
    If this PR closes any issues, be sure to add "closes #<number>"
    somewhere.
    -->
    
    ## Performance / NEAR gas cost considerations
    
    <!--
    Performance regressions are not ideal, though we welcome performance 
    improvements. Any PR must be completely mindful of any gas cost
    increases. The
    CI will fail if the gas costs change at all. Do update these tests to 
    accommodate for the new gas changes. It is good to explain 
    this change, if necessary.
    -->
    
    ## Testing
    Some tests have been added to test the pause and unpause logic.
    <!--
    Please describe the tests that you ran to verify your changes.
    -->
    
    ## How should this be reviewed
    
    <!--
    Include any recommendations of areas to be careful of to ensure that the
    reviewers use extra attention.
    -->
    
    ## Additional information
    
    <!--
    Include any additional information which you think should be in this PR,
    such
    as prior arts, future extensions, unresolved problems, or a TODO list
    which
    should be followed up.
    -->

--- engine/src/errors.rs
+++ engine/src/errors.rs
@@ -24,6 +24,7 @@ pub const ERR_NO_UPGRADE: &[u8; 14] = b"ERR_NO_UPGRADE";
 pub const ERR_NOT_ALLOWED: &[u8; 15] = b"ERR_NOT_ALLOWED";
 pub const ERR_NOT_OWNER: &[u8; 13] = b"ERR_NOT_OWNER";
 pub const ERR_PAUSED: &[u8; 10] = b"ERR_PAUSED";
+pub const ERR_FT_PAUSED: &[u8; 13] = b"ERR_FT_PAUSED";
 pub const ERR_RUNNING: &[u8; 11] = b"ERR_RUNNING";
 
 pub const ERR_SERIALIZE: &str = "ERR_SERIALIZE";
