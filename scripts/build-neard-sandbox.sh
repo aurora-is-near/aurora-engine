71c4e453005e8fc07b432187e68d43b93bf92436
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2024-09-05 10:59:49 +0100

    ci: fix path to neard-sandbox

--- scripts/build-neard-sandbox.sh
+++ scripts/build-neard-sandbox.sh
@@ -4,7 +4,7 @@ TAG=`curl -s https://rpc.${NETWORK}.near.org/status  | jq -r .version.build`
 git clone https://github.com/near/nearcore -b $TAG
 cd nearcore
 make sandbox
+mv $(find target -name neard-sandbox) ../
 cd ..
-mv ${PWD}/nearcore/target/release/neard-sandbox .
 rm -rf nearcore
 echo "NEAR_SANDBOX_BIN_PATH=${PWD}/neard-sandbox" >> $GITHUB_ENV
