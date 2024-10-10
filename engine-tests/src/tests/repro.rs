5bd7b6bb97d35d64f7642319d5694188ee314f85
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2024-10-10 08:49:01 +0100

    chore: release notes 3.7.0 (#964)
    ## Description
    
    The release notes for upcoming Release 3.7.0

--- engine-tests/src/tests/repro.rs
+++ engine-tests/src/tests/repro.rs
@@ -71,7 +71,10 @@ fn repro_FRcorNv() {
         block_timestamp: 1_650_960_438_774_745_116,
         input_path: "src/tests/res/input_FRcorNv.hex",
         evm_gas_used: 1_239_721,
+        #[cfg(not(feature = "ext-connector"))]
         near_gas_used: 168,
+        #[cfg(feature = "ext-connector")]
+        near_gas_used: 167,
     });
 }
 
