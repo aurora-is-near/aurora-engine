784e1cedb73324ba8c61a56d091a8ad1ba1769f9
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2024-11-16 11:46:22 +0000

    cache folder for yarn install

--- engine-tests/src/utils/one_inch/mod.rs
+++ engine-tests/src/utils/one_inch/mod.rs
@@ -35,6 +35,9 @@ fn download_and_compile_solidity_sources(repo_name: &str) -> PathBuf {
     // install packages
     let output = Command::new("/usr/bin/env")
         .current_dir(&sources_dir)
+        // The `--cache-folder` argument should be provided because there could be a case when
+        // two instances of yarn are running in parallel, and they are trying to install
+        // the same dependencies.
         .args(["yarn", "install", "--cache-folder", repo_name])
         .output()
         .unwrap();
