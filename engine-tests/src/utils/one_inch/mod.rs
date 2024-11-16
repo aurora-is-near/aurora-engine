9ba3a5f377132c5466eb893490e8500ebce960d8
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2024-11-16 12:21:44 +0000

    ci: bump node up to 18 in scheduled script (#975)
    ## Description
    
    The PR bumps node up to 18 in the ci script for scheduled tasks.

--- engine-tests/src/utils/one_inch/mod.rs
+++ engine-tests/src/utils/one_inch/mod.rs
@@ -14,6 +14,8 @@ pub static LIMIT_ORDER_PROTOCOL_PATH: LazyLock<PathBuf> =
 
 fn download_and_compile_solidity_sources(repo_name: &str) -> PathBuf {
     let sources_dir = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
         .join("target")
         .join(repo_name);
     // Contracts not already present, so download and compile them (but only once, even
@@ -33,7 +35,10 @@ fn download_and_compile_solidity_sources(repo_name: &str) -> PathBuf {
     // install packages
     let output = Command::new("/usr/bin/env")
         .current_dir(&sources_dir)
-        .args(["yarn", "install"])
+        // The `--cache-folder` argument should be provided because there could be a case when
+        // two instances of yarn are running in parallel, and they are trying to install
+        // the same dependencies.
+        .args(["yarn", "install", "--cache-folder", repo_name])
         .output()
         .unwrap();
     assert!(
