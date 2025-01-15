7d610748ab72644a6a89dd6dae64625f5ce50bd5
Evgeny Ukhanov <evgeny@aurora.dev>
2025-01-16 00:12:47 +0100

    Update gas for tests

--- engine-tests/src/tests/uniswap.rs
+++ engine-tests/src/tests/uniswap.rs
@@ -39,7 +39,7 @@ fn test_uniswap_input_multihop() {
 
     let (_amount_out, _evm_gas, profile) = context.exact_input(&tokens, INPUT_AMOUNT.into());
 
-    assert_eq!(107, profile.all_gas() / 1_000_000_000_000);
+    assert_eq!(117, profile.all_gas() / 1_000_000_000_000);
 }
 
 #[test]
@@ -59,7 +59,7 @@ fn test_uniswap_exact_output() {
 
     let (_amount_in, profile) =
         context.exact_output_single(&token_a, &token_b, OUTPUT_AMOUNT.into());
-    utils::assert_gas_bound(profile.all_gas(), 17);
+    utils::assert_gas_bound(profile.all_gas(), 18);
     let wasm_fraction = 100 * profile.wasm_gas() / profile.all_gas();
     assert!(
         (38..=48).contains(&wasm_fraction),
