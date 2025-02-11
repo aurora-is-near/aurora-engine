d7e52b2585fb18a1a218540ccfca325fe0f85e1b
Evgeny Ukhanov <evgeny@aurora.dev>
2025-02-11 20:04:49 +0100

    Added to engine-sdk BLS12-381 host functions

--- engine-sdk/src/near_runtime.rs
+++ engine-sdk/src/near_runtime.rs
@@ -643,6 +643,23 @@ pub(crate) mod exports {
         pub(crate) fn alt_bn128_g1_sum(value_len: u64, value_ptr: u64, register_id: u64);
         pub(crate) fn alt_bn128_g1_multiexp(value_len: u64, value_ptr: u64, register_id: u64);
         pub(crate) fn alt_bn128_pairing_check(value_len: u64, value_ptr: u64) -> u64;
+        pub(crate) fn bls12381_p1_sum(value_len: u64, value_ptr: u64, register_id: u64) -> u64;
+        pub(crate) fn bls12381_p2_sum(value_len: u64, value_ptr: u64, register_id: u64) -> u64;
+        pub(crate) fn bls12381_g1_multiexp(value_len: u64, value_ptr: u64, register_id: u64)
+            -> u64;
+        pub(crate) fn bls12381_g2_multiexp(value_len: u64, value_ptr: u64, register_id: u64)
+            -> u64;
+        pub(crate) fn bls12381_map_fp_to_g1(
+            value_len: u64,
+            value_ptr: u64,
+            register_id: u64,
+        ) -> u64;
+        pub(crate) fn bls12381_map_fp2_to_g2(
+            value_len: u64,
+            value_ptr: u64,
+            register_id: u64,
+        ) -> u64;
+        pub(crate) fn bls12381_pairing_check(value_len: u64, value_ptr: u64) -> u64;
         // #####################
         // # Miscellaneous API #
         // #####################
