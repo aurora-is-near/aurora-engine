13b921b534127886d7ae6a7ffd8980e4f6d5bc40
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2025-05-15 18:03:59 +0100

    chore: modify install wasm-opt

--- scripts/ci-deps/dind.sh
+++ scripts/ci-deps/dind.sh
@@ -3,3 +3,4 @@
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install -y yarn build-essential pkg-config libclang-dev libssl-dev
+sudo scripts/ci-deps/install-wasm-opt.sh
