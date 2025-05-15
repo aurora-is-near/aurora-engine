386a9f28915a1dd69b21e96d64a9fdeb4da911fe
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2025-05-15 18:18:07 +0100

    chore: install rust nightly toolchain

--- scripts/ci-deps/native.sh
+++ scripts/ci-deps/native.sh
@@ -2,8 +2,8 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-#apt update
-#apt install -y build-essential pkg-config libclang-dev libssl-dev gnupg curl git
+apt update
+apt install -y build-essential pkg-config libclang-dev libssl-dev gnupg curl git
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt update
