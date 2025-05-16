f107c049d4911fb514a291c763abe2ade022cff2
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2025-05-16 17:20:13 +0100

    ci: use k8 self-hosted runners (#974)
    ## Description
    
    The PR changes the usage of legacy self-hosted runners to the new ones,
    k8, for some tasks. The tasks which run tests are still using GitHub
    runners, since on the k8 runners, the tests fail from time to time
    (probably because of sharing resources between workers).
    
    ---------
    
    Co-authored-by: kogeler <kogeler@gmail.com>

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rustup toolchain add stable
 cargo +stable install --no-default-features --force cargo-make
-scripts/ci/install-wasm-opt.sh
-cargo make --profile "$1" build-docker-inner
+scripts/ci-deps/install-wasm-opt.sh
+cargo make build-docker-inner
