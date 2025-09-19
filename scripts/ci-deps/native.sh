e9343c69a12e63984e106c19ef256baf5a14a5b3
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2025-09-19 17:46:02 +0100

    ci: install yarn with apt gpg #2

--- scripts/ci-deps/native.sh
+++ scripts/ci-deps/native.sh
@@ -8,8 +8,8 @@ apt install -y build-essential pkg-config libclang-dev libssl-dev gnupg curl git
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor -o /etc/apt/keyrings/yarn.gpg
 chmod a+r /etc/apt/keyrings/yarn.gpg
-echo "deb [signed-by=/etc/apt/keyrings/yarn.gpg] https://dl.yarnpkg.com/debian stable main" \
-  | tee /etc/apt/sources.list.d/yarn.list >/dev/null
+echo "deb [signed-by=/etc/apt/keyrings/yarn.gpg] https://dl.yarnpkg.com/debian stable main" | tee /etc/apt/sources.list.d/yarn.list >/dev/null
+apt update
 apt install -y yarn
 yarn install
 scripts/ci-deps/install-wasm-opt.sh
