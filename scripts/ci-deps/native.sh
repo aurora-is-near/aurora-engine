240670e04528ce24282c7526ea9ce3f8f5a213dc
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2025-09-19 17:24:06 +0100

    ci: install yarn via npm

--- scripts/ci-deps/native.sh
+++ scripts/ci-deps/native.sh
@@ -4,6 +4,9 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt update
 apt install -y build-essential pkg-config libclang-dev libssl-dev gnupg curl git
-curl -o- -L https://yarnpkg.com/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 18
+npm install --global yarn
 yarn install
 scripts/ci-deps/install-wasm-opt.sh
