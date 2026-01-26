da48f256d46ee854f591a4c5e7917273c4b2f3c5
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2026-01-26 10:38:03 +0000

    fix: install wasm-opt

--- scripts/ci-deps/native.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export DEBIAN_FRONTEND=noninteractive
-
-apt update
-apt install -y build-essential pkg-config libclang-dev libssl-dev gnupg curl git gpg
-scripts/ci-deps/install-wasm-opt.sh
