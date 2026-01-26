1fc4267b501d04ce813d7b73ca010838c7a0c83d
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2026-01-26 09:58:11 +0000

    fix: install yarn

--- scripts/ci-deps/dind.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt update && sudo apt install -y yarn build-essential pkg-config libclang-dev libssl-dev
-sudo scripts/ci-deps/install-wasm-opt.sh
