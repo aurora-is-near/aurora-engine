e461051e7b197e5c2e13ef92cfd98ea052e4a090
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2026-05-12 17:28:08 +0100

    chore: remove unused AuthorizationListTooLarge error type

--- engine-transactions/src/lib.rs
+++ engine-transactions/src/lib.rs
@@ -379,7 +379,6 @@ pub enum Error {
     RlpDecodeError(DecoderError),
     UnsupportedTransactionEip4844,
     EmptyAuthorizationList,
-    AuthorizationListTooLarge,
 }
 
 #[cfg(feature = "serde")]
@@ -401,7 +400,6 @@ impl Error {
             Self::RlpDecodeError(_) => "ERR_TX_RLP_DECODE",
             Self::UnsupportedTransactionEip4844 => "ERR_UNSUPPORTED_TX_EIP4844",
             Self::EmptyAuthorizationList => "ERR_EMPTY_AUTHORIZATION_LIST",
-            Self::AuthorizationListTooLarge => "ERR_AUTHORIZATION_LIST_TOO_LARGE",
         }
     }
 }
