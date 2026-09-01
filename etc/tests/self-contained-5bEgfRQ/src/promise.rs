4cda479a53469039a7672278b37a72c4e1c2ae1e
Oleksandr Anyshchenko <oleksandr.anyshchenko@aurora.dev>
2026-09-01 13:17:22 +0100

    chore: update dependencies and migrate to Rust 2024 edition
    - Bumped crate dependencies (e.g., `near-primitives`, `syn`, `base64`, etc.).
    - Updated all projects to use the Rust 2024 edition.
    - Enhanced safety annotations by replacing `#[no_mangle]` with `#[unsafe(no_mangle)]` in specific functions.

--- etc/tests/self-contained-5bEgfRQ/src/promise.rs
+++ etc/tests/self-contained-5bEgfRQ/src/promise.rs
@@ -2,10 +2,13 @@ use aurora_engine_sdk::promise::{PromiseHandler, PromiseId};
 use aurora_engine_types::parameters::{PromiseBatchAction, PromiseCreateArgs};
 use aurora_engine_types::types::PromiseResult;
 
-/// A promise handler which does nothing. Should only be used when promises can be safely ignored.
+/// A promise handler that does nothing. Should only be used when promises can be safely ignored.
+#[derive(Copy, Clone)]
 pub struct Noop;
 
 impl PromiseHandler for Noop {
+    type ReadOnly = Self;
+
     fn promise_results_count(&self) -> u64 {
         0
     }
@@ -31,4 +34,20 @@ impl PromiseHandler for Noop {
     }
 
     fn promise_return(&mut self, _promise: PromiseId) {}
+
+    fn promise_create_and_combine(&mut self, _args: &[PromiseCreateArgs]) -> PromiseId {
+        PromiseId::new(0)
+    }
+
+    fn promise_attach_batch_callback(
+        &mut self,
+        _base: PromiseId,
+        _args: &PromiseBatchAction,
+    ) -> PromiseId {
+        PromiseId::new(0)
+    }
+
+    fn read_only(&self) -> Self::ReadOnly {
+        *self
+    }
 }
